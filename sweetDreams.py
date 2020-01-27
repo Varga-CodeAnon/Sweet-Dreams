42f181a72afcb7c39d27931e455b9766c646f905
Varga-CodeAnon <editionpirated@protonmail.com>
2020-01-27 19:54:52 +0100

    Scanning time measurement

--- sweetDreams.py
+++ sweetDreams.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import time
 import sys
 
 if len(sys.argv) != 3:
@@ -7,5 +8,9 @@ if len(sys.argv) != 3:
 
 target = sys.argv[1]
 file_o = open(sys.argv[2], "w")
+start_time = time.time()
+time.sleep(3)
+end_time = time.time()
 # file_o.write("Coucou\n")
 file_o.close
+print("[*] Done, ", end_time-start_time," elapsed !")
