bcdeea717ef968f8e0c9c12e7d0bbe0a932c8bc7
Varga-CodeAnon <editionpirated@protonmail.com>
2020-01-28 20:01:53 +0100

    Better with a function

--- sweetDreams.py
+++ sweetDreams.py
@@ -3,6 +3,17 @@ import time
 import sys
 import subprocess
 
+def service_catcher(file_name):
+    """Catch from a nmap grepable output file the list of actives services"""
+    scan_f = open("scan.txt","r")
+    for line in scan_f:
+        if "Ports:" in line:
+            services = line.split('//')[1:-1] # Split the output and only keep the ports fields
+            del services[1::2] # Only keep the service name
+    
+    scan_f.close
+    return services
+
 if len(sys.argv) != 3:
     sys.stderr.write("Argument error : ./sweetDreams.py <target> <output_file.xml>\n")
     exit(1)
@@ -11,19 +22,9 @@ target = sys.argv[1]
 file_o = open(sys.argv[2], "w")
 start_time = time.time()
 
-# Let start the first scan
-# [...]
-
-# Protocols list
-scan_f = open("scan.txt","r")
-for line in scan_f:
-    if "Ports:" in line:
-        protocols = line.split('//')[1:-1] # Split the output and only keep the ports fields
-        del protocols[1::2] # Only keep the protocol name
-# print(protocols)
-scan_f.close
-# --------------
-
+# TODO: multiple output ? On garde le -oG pour la liste servicee, mais en même temps dans un autre fichier on met le -oX
+service_l = service_catcher("scan.txt")
+print(service_l)
 end_time = time.time()
 # file_o.write("Coucou\n")
 file_o.close
