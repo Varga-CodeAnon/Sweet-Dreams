71bf3f89e5f323384d3112a1475b6292792c0bea
Varga-CodeAnon <editionpirated@protonmail.com>
2020-01-28 19:44:40 +0100

    Add actives protocols list

--- sweetDreams.py
+++ sweetDreams.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import time
 import sys
+import subprocess
 
 if len(sys.argv) != 3:
     sys.stderr.write("Argument error : ./sweetDreams.py <target> <output_file.xml>\n")
@@ -9,8 +10,22 @@ if len(sys.argv) != 3:
 target = sys.argv[1]
 file_o = open(sys.argv[2], "w")
 start_time = time.time()
-time.sleep(3)
+
+# Let start the first scan
+# [...]
+
+# Protocols list
+scan_f = open("scan.txt","r")
+for line in scan_f:
+    if "Ports:" in line:
+        protocols = line.split('//')[1:-1] # Split the output and only keep the ports fields
+        del protocols[1::2] # Only keep the protocol name
+# print(protocols)
+scan_f.close
+# --------------
+
 end_time = time.time()
 # file_o.write("Coucou\n")
 file_o.close
 print("[*] Done, ", end_time-start_time," elapsed !")
+
