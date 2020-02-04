38bbfcfb28db9153228b79bde526df88efc54899
Varga-CodeAnon <editionpirated@protonmail.com>
2020-02-04 22:38:32 +0100

    Add a table in the cherry tree main node

--- sweetDreams.py
+++ sweetDreams.py
@@ -123,16 +123,22 @@ def cherry_header(file_ctd,target,op_sys):
     file_ctd.write(
 """<?xml version="1.0" ?>
 <cherrytree>
-    <node custom_icon_id="14" foreground="" is_bold="True" name="SweetDreams" prog_lang="custom-colors" readonly="False" tags="" ts_creation="0.0" ts_lastsave="0.0" unique_id="1">\
+    <node custom_icon_id="14" foreground="" is_bold="True" name="SweetDreams" prog_lang="custom-colors" readonly="False" tags="" ts_creation="0.0" ts_lastsave="0.0" unique_id="1">
         <rich_text weight="heavy">Target:</rich_text>
         <rich_text> """ + target + """\n</rich_text>
         <rich_text weight="heavy">OS:</rich_text>
-		<rich_text> """ + op_sys + """\n</rich_text>""")
+		<rich_text> """ + op_sys + """\n</rich_text>
+        <rich_text weight="heavy">Services:</rich_text>
+		<rich_text>\n</rich_text>
+        <rich_text justification="left"></rich_text>
+		<table char_offset="66" col_max="200" col_min="40">""")
+        # rows & cells
+    file_ctd.write("\n        </table>")
 
 def cherry_tail(file_ctd):
     """Write a cherrytree end of file (xml format)"""
     file_ctd.write(
-"""	</node>
+"""\n    </node>
 </cherrytree>""")
 
 # =========================== MAIN ===========================
@@ -173,5 +179,7 @@ cherry_tail(file_o)
 
 # /////////////////////////////////////
 end_time = time.time()
+hours, rem = divmod(end_time-start_time, 3600)
+minutes, seconds = divmod(rem, 60)
+print("[*] Done, {:0>2}:{:0>2}:{:05.2f} elapsed !".format(int(hours),int(minutes),seconds))
 # file_o.close
-print("[*] Done, ", end_time-start_time," elapsed !")
