8a14e3a8746c630d71ce6f1c0c7b8ada4888f73d
Varga-CodeAnon <editionpirated@protonmail.com>
2020-02-07 15:36:29 +0100

    Import the modules

---
+++ modules/ssh.py
@@ -0,0 +1,7 @@
+def main_ssh():
+    """"""
+    print("[*] SSH enum in progress...")
+    # node_file = cherry_node_head(port,serv,versions[i])  # TODO: à implémenter
+    # # import 80.py ou exec 80.py
+    # cherry_node_tail(node_file)
+    # insert_node(node_file,filename)
--- sweetDreams.py
+++ sweetDreams.py
@@ -3,9 +3,13 @@
 import datetime
 import itertools
 import os
+import os.path
 import subprocess
 import sys
 import time
+from modules.cherrytree import *
+from modules.ssh import *
+from modules.http import *
 
 
 def error_display(code):
@@ -121,50 +125,6 @@ def os_guess(sv_file):
     return os_version
 
 
-def cherry_table(file_ctd,port_serv,versions):
-    """Write a table inside a cherrytree file"""
-    file_ctd.write(
-        """\n        <rich_text justification="left"></rich_text>
-		<table char_offset="66" col_max="200" col_min="40">""")
-    for i,(port,serv) in enumerate(port_serv.items()):
-        if serv == '':
-            serv = 'Not found...'
-        file_ctd.write(
-"""\n            <row>
-                <cell>"""+port+"""</cell>
-                <cell>"""+serv+"""</cell>
-                <cell>"""+versions[i]+"""</cell>
-            </row>""")
-    file_ctd.write(
-"""\n            <row>
-				<cell>Port</cell>
-				<cell>Protocol</cell>
-				<cell>Version</cell>
-			</row>
-        </table>""")
-
-
-def cherry_header(file_ctd,target,op_sys):
-    """Write a cherrytree header (xml format)"""
-    file_ctd.write(
-"""<?xml version="1.0" ?>
-<cherrytree>
-    <node custom_icon_id="14" foreground="" is_bold="True" name="SweetDreams" prog_lang="custom-colors" readonly="False" tags="" ts_creation="0.0" ts_lastsave="0.0" unique_id="1">
-        <rich_text weight="heavy">Target:</rich_text>
-        <rich_text> """ + target + """\n</rich_text>
-        <rich_text weight="heavy">OS:</rich_text>
-		<rich_text> """ + op_sys + """\n</rich_text>
-        <rich_text weight="heavy">Services:</rich_text>
-		<rich_text>\n\n</rich_text>""")
-    
-
-def cherry_tail(file_ctd):
-    """Write a cherrytree end of file (xml format)"""
-    file_ctd.write(
-"""\n    </node>
-</cherrytree>\n""")
-
-
 # =========================== MAIN ===========================
 if len(sys.argv) != 3:
     error_display(1)
@@ -186,17 +146,10 @@ file_o = open(file_name, "a")
 cherry_header(file_o,target,os)
 cherry_table(file_o, port_serv, versions)
 # --- BODY ---
-# for i,port,serv in enumerate(port_serv.items()):
-    # node_file = cherry_node_head(port,serv,versions[i])  # TODO: à implémenter
-    # import 80.py ou exec 80.py
-    # cherry_node_tail(node_file)
-    # insert_node(node_file,filename)
-    # TODO: Body
-    # Pour chaque clé du dico:
-    #   créer un noeud dont l'entête est le résultat de sV (l'indice de la version est le même que celui de la clé correspondante)
-    #   Puis, remplir sous l'entête avec 22.py, 80.py
-    #   Enfin, conclure avec le tail du noeud
-
+for i,(port,serv) in enumerate(port_serv.items()):
+    function_name = serv + ".py"
+    if os.path.isfile("modules/" + function_name):
+        exec(function_name)
 # --- TAIL ---
 cherry_tail(file_o)
 # /////////////////////////////////////
