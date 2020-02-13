7b98020a3efa09f8945c1e705c2dedd9579e24e6
Varga-CodeAnon <editionpirated@protonmail.com>
2020-02-13 14:37:32 +0100

    Add fingerprint and cve nodes

--- modules/ssh.py
+++ modules/ssh.py
@@ -2,13 +2,75 @@
 
 from modules.cherrytree import *
 
+
+def description(file_ctd):
+    """TODO:"""
+    file_ctd.write(
+"""\n        <rich_text style="italic" weight="heavy">CheckList:</rich_text>
+			<rich_text>
+
+• Fingerprint server </rich_text>
+			<rich_text foreground="#2a30ffff0000">(done)</rich_text>
+			<rich_text>
+   ◇ telnet ip_address 22 (banner grab) 
+   ◇ </rich_text>
+			<rich_text link="webs http://www.monkey.org/%7Eprovos/scanssh/">scanssh</rich_text>
+			<rich_text>
+      ▪ scanssh -p -r -e excludes random(no.)/Network_ID/Subnet_Mask 
+• Password guessing
+   ◇ ssh root@ip_address 
+   ◇ </rich_text>
+			<rich_text link="webs http://packetstormsecurity.org/groups/teso/guess-who-0.44.tgz">guess-who</rich_text>
+			<rich_text>
+      ▪ ./b -l username -h ip_address -p 22 -2 &lt; password_file_location 
+   ◇ </rich_text>
+			<rich_text link="webs http://freeworld.thc.org/">Hydra brute force </rich_text>
+			<rich_text>
+   ◇ </rich_text>
+			<rich_text link="webs http://www.edge-security.com/edge-soft.php">brutessh</rich_text>
+			<rich_text>
+   ◇ </rich_text>
+			<rich_text link="webs http://www.darkc0de.com/c0de/ruby/sshbrute.txt">Ruby SSH Bruteforcer </rich_text>
+			<rich_text>                                                                
+• Examine configuration files 
+   ◇ ssh_config 
+   ◇ sshd_config 
+   ◇ authorized_keys 
+   ◇ ssh_known_hosts 
+   ◇ .shosts 
+• SSH Client programs
+   ◇ </rich_text>
+			<rich_text link="webs http://www.bitvise.com/">tunnelier </rich_text>
+			<rich_text>
+   ◇ </rich_text>
+			<rich_text link="webs http://www.bitvise.com/winsshd">winsshd </rich_text>
+			<rich_text>
+   ◇ </rich_text>
+			<rich_text link="webs http://www.chiark.greenend.org.uk/%7Esgtatham/putty/">putty </rich_text>
+			<rich_text>
+   ◇ </rich_text>
+			<rich_text link="webs http://winscp.net/eng/index.php">winscp </rich_text>
+			<rich_text>
+
+</rich_text>"""
+    )
+
+
 def main_ssh(file_ctd):
     """Execute the information gathering"""
-    begin_node(file_ctd,"ssh")
+    begin_node(file_ctd,"ssh",'2')
     print("[*] SSH enum in progress...")
+    description(file_ctd)
+    # cve
+    begin_node(file_ctd,"cve",'3')
+    end_node(file_ctd)
+    # /cve
+    # fingerprint
+    begin_node(file_ctd,"fingerprint",'4')
+    file_ctd.write("""\n
+                <codebox char_offset="0" frame_height="175" frame_width="750" highlight_brackets="True" show_line_numbers="False" syntax_highlighting="sh" width_in_pixels="True">OK
+BOOMER""") FIXME:
+    end_node(file_ctd)
+    # /fingerprint
     end_node(file_ctd)
     print("[*] SSH enum done!")
-    # node_file = cherry_node_head(port,serv,versions[i])  # TODO: à implémenter
-    # # import 80.py ou exec 80.py
-    # cherry_node_tail(node_file)
-    # insert_node(node_file,filename)
